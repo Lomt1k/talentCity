b082e47969a4a1763e23d3df9167e235904b9beb
Lomt1k <yashin2236@gmail.com>
2021-11-07 16:11:17 +0600

    up version to 1.1

---
+++ Assets/Scripts/VersionView.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class VersionView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _versionText;
+
+    void Start()
+    {
+        _versionText.text = "version " + Application.version;
+    }
+}
