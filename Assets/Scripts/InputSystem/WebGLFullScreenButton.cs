b8a5b08c3e93d8bd189598a0be4cf80eaf8d9bbe
Lomt1k <yashin2236@gmail.com>
2022-01-24 20:43:15 +0300

    fix WebGL

---
+++ Assets/Scripts/InputSystem/WebGLFullScreenButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WebGLFullScreenButton : MonoBehaviour
+{
+    private void Start()
+    {
+        #if !PLATFORM_WEBGL || UNITY_EDITOR
+        gameObject.SetActive(false);
+        #endif
+    }
+
+    //unity event
+    public void OnClickFullScreen()
+    {
+        Screen.fullScreen = true;
+        gameObject.SetActive(false);
+    }
+
+    //unity event
+    public void OnClickWindowed()
+    {
+        gameObject.SetActive(false);
+    }
+}
